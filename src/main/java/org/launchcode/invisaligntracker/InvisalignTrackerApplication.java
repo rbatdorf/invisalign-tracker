9b2d245c62a96671d8a47fa66b8b2dd495e22c40
Robert Batdorf <robert.batdorf@gmail.com>
2018-04-08 14:38:21 -0500

    Initial commit

---
+++ src/main/java/org/launchcode/invisaligntracker/InvisalignTrackerApplication.java
@@ -0,0 +1,12 @@
+package org.launchcode.invisaligntracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvisalignTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvisalignTrackerApplication.class, args);
+	}
+}
---
+++ src/main/java/org/launchcode/invisaligntracker/controllers/HelloController.java
@@ -0,0 +1,26 @@
+package org.launchcode.invisaligntracker.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.xml.ws.RequestWrapper;
+
+@Controller
+public class HelloController {
+
+    @RequestMapping(value = "")
+    @ResponseBody
+    //because no html template is created//
+    public String index() {
+
+        return "Hello World";
+    }
+
+    @RequestMapping(value = "goodbye")
+    @ResponseBody
+    public String goodbye(){
+        return "goodbye";
+    }
+
+}
